744a683e9e5723bbecdd548baf96f64e4b188be1
ramen <ramen.dey@creativemileage.com>
2015-11-04 21:48:26 +0530

    Header & Footer added

---
+++ themes/activity/views/layouts/footer.php
@@ -0,0 +1,137 @@
+<footer class="site-footer" role="contentinfo">
+        <div class="footer-paralux">
+            <h4> Join our Monthly Membership </h4>
+            <h3> Take any class at your free time flexibly for whole month </h3>
+            <a class="btn btn-black btn-small btn-transparent" href="#" hidefocus="true" style="outline: medium none;">
+<span>Sing up now</span>
+</a>
+        </div>
+
+        <div class="footer_widget"> 
+          <div class="container footer-div">
+           <div class="col-sm-4">
+              <h4> Addrss </h4>
+              
+                  <div class="contact-address details">
+                    <ul class="media-list">
+                  <li class="media description">
+                    <div class="media-left">
+                      <i class="fa fa-map-marker"></i>
+                    </div>
+                    <div class="media-body">
+                      <p>Lorem Ipsum is simply dummy text of the printing  </p>
+                    </div>
+                  </li>
+                  <li class="media description">
+                    <div class="media-left">
+                      <i class="fa fa-phone"></i>
+                    </div>
+                    <div class="media-body">
+                      <p>+48 123 456 789 </p>
+                    </div>
+                  </li>
+                  <li class="media description">
+                    <div class="media-left ">
+                      <i class="fa fa-envelope-o"></i>
+                    </div>
+                    <div class="media-body">
+                      <p>Info@test.com </p>
+                    </div>
+                  </li>
+                  <li class="media description">
+                    <div class="media-left ">
+                      <i class="fa fa-skype"></i>
+                    </div>
+                    <div class="media-body">
+                      <p>Your Company</p>
+                    </div>
+                  </li>
+                </ul>
+                
+                  <div class="socail_icon"> 
+                     <ul>
+                        <li><a href="#"> <i class="fa fa-facebook"></i></a> </li>
+                        <li><a href="#"> <i class="fa fa-twitter"></i></a> </li>
+                        <li><a href="#"> <i class="fa fa-google-plus"></i></a> </li>
+                        <li><a href="#"> <i class="fa fa-pinterest-p"></i> </a></li>
+                     </ul>
+                  </div>
+                
+                  </div>
+              
+           </div>
+          <!-- <div class="col-sm-3 twitter_div">
+              <h4> Recent Tweets </h4>
+                 
+                 <ul class="media-list">
+                  <li class="media">
+                    <div class="media-left">
+                      <i class="fa fa-twitter"></i>
+                    </div>
+                    <div class="media-body">
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of </p>
+                    </div>
+                  </li>
+                </ul>
+                
+                <ul class="media-list">
+                  <li class="media">
+                    <div class="media-left">
+                      <i class="fa fa-twitter"></i>
+                    </div>
+                    <div class="media-body">
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of </p>
+                    </div>
+                  </li>
+                </ul>
+                 
+            </div> -->
+           <div class="col-sm-4 about-footer">
+              <h4> About </h4>
+                
+                <p>Description about the site</p>
+             <!-- <ul>
+                 <li><a href="#"> My Account </a></li>
+                 <li><a href="#"> View Order </a></li>
+                 <li><a href="#"> Privacy and Security </a></li>
+                 <li><a href="#"> Promotions </a></li>
+                 <li><a href="#"> About Us </a></li>
+                 <li><a href="#"> Contact Us </a></li>
+              </ul> -->
+              
+            </div>
+           <div class="col-sm-4">
+              <h4> Learn More </h4>
+              
+               <ul>
+                 <li><a href="#"> FAQ </a></li>
+                 <li><a href="#"> Terms </a></li>
+                 <li><a href="#"> Privacy and Security </a></li>
+                 <li><a href="#"> Partners </a></li>
+                 <li><a href="#"> Blog </a></li>
+                 <li><a href="#"> Press </a></li>
+              </ul>
+              
+          <!-- <form id="contact-form" class="contact-form form-dark" action="#" method="post">
+            <div class="field-text">
+            <input id="name" type="text" placeholder="NAME" value="" name="name" hidefocus="true" style="outline: medium none;">
+            </div>
+            <div class="field-text">
+            <input id="email" type="email" placeholder="EMAIL" value="" required name="email" hidefocus="true" style="outline: medium none;">
+            </div>
+            <div class="clearfix"></div>
+            <div class="field-text">
+            <textarea id="message" placeholder="MESSAGE" required name="message" hidefocus="true" style="outline: medium none;"></textarea>
+            </div>
+            <span class="btn btn-padding-big pull-left">
+            <input type="submit" value="Send" hidefocus="true" style="outline: medium none;">
+            </span>
+            </form> -->
+              
+            </div>
+          </div>   
+        </div>
+
+        <div class="copyright">Copyright © 2015. activity All rights reserved</div>
+    </footer>
+    <!-- Site Footer -->
